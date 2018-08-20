c8f1192535df14d423e3832610778d8a30657fb4
Pawan P Singh <psingh0912@gmail.com>
2018-08-20 16:24:02 +0530

    Create roles.rb

---
+++ roles.rb
@@ -0,0 +1,11 @@
+roles = [
+  'Admin',
+  'Supervisor - Sales',
+  'Staff - Sales',
+  'Supervisor - IFR',
+  'Staff - IFR',
+  'Supervisor - Feasibility',
+  'Staff - Feasibility',
+  'Supervisor - Provisioning',
+  'Staff - Provisoning'
+]
